831f0b9cd2f03161a03fe52a786c765a8487834f
hcaula <henrique.caula@hotmail.com>
2018-04-28 19:32:51 -0300

    Creates controllers, models and routes directories if they don't exist

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,9 @@
 */
 const app_dir_name = 'app';
 const controllers_dir_name = 'controllers';
-const models_dir_name = 'controllers';
+const models_dir_name = 'models';
 const routes_dir_name = 'routes';
-const has_routes = false;
+const separated_routes = false;
 
 /*
  * Modules
@@ -19,7 +19,13 @@ const fs = require('fs');
  * Main
 */
 const main_dir = process.cwd();
-const app_dir = path.join(main_dir, 'app');
+const app_dir = path.join(main_dir, app_dir_name);
+const controllers_dir = path.join(app_dir, controllers_dir_name);
+const models_dir = path.join(app_dir, models_dir_name);
+const routes_dir = path.join(app_dir, routes_dir_name);
 
-/* Creates app directory if it doesn't exist */
+/* Creates directories if they don't exist */
 if (!fs.existsSync(app_dir)) fs.mkdirSync(app_dir);
+if (!fs.existsSync(controllers_dir)) fs.mkdirSync(controllers_dir);
+if (!fs.existsSync(models_dir)) fs.mkdirSync(models_dir);
+if (!fs.existsSync(routes_dir) && separated_routes) fs.mkdirSync(routes_dir);
