ace9312b30318c2dbb5b13383e12c38031a76484
hcaula <henrique.caula@hotmail.com>
2018-04-28 19:28:04 -0300

    Creates 'app' directory if it doesn't exist

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-const app_dir = process.cwd();
+
+/*
+ * Inputs
+*/
+const app_dir_name = 'app';
+const controllers_dir_name = 'controllers';
+const models_dir_name = 'controllers';
+const routes_dir_name = 'routes';
+const has_routes = false;
+
+/*
+ * Modules
+*/
+const path = require('path');
+const fs = require('fs');
+
+/*
+ * Main
+*/
+const main_dir = process.cwd();
+const app_dir = path.join(main_dir, 'app');
+
+/* Creates app directory if it doesn't exist */
+if (!fs.existsSync(app_dir)) fs.mkdirSync(app_dir);
